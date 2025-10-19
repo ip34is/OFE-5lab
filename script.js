983700c1366aa873614846acf8682a2cee704671
ivskachkov <159800748+ip34is@users.noreply.github.com>
2025-10-19 20:18:31 +0300

    upd

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.getElementById('validationForm');
-    if (form) {
-        const fields = {
-            fullName: /^[А-ЯІЇЄҐ][а-яіїєґ']+\s[А-ЯІЇЄҐ]\.[А-ЯІЇЄҐ]\.$/,
-            idCard: /^[А-ЯІЇЄҐ]{2}\s№\d{6}$/,
-            faculty: /^[А-ЯІЇЄҐ]{4}$/,
-            birthDate: /^(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.(19\d\d|20\d\d)$/,
-            address: /^м\.\s[А-ЯІЇЄҐ][а-яіїєґ']+$/
-        };
+    const fields = {
+        fullName: /^[А-ЯІЇЄҐ][а-яіїєґ']+\s[А-ЯІЇЄҐ]\.[А-ЯІЇЄҐ]\.$/,
+        idCard: /^[А-ЯІЇЄҐ]{2}\s№\d{6}$/,
+        faculty: /^[А-ЯІЇЄҐ]{4}$/,
+        birthDate: /^(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.(19\d\d|20\d\d)$/,
+        address: /^м\.\s[А-ЯІЇЄҐ][а-яіїєґ']+$/
+    };
 
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            let allValid = true;
-            let formData = '';
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let allValid = true;
+        let formData = '';
 
-            Object.keys(fields).forEach(id => {
-                const input = document.getElementById(id);
-                if (input) {
-                    input.classList.remove('invalid');
-                }
-            });
-
-            for (const id in fields) {
-                const input = document.getElementById(id);
-                const regex = fields[id];
-
-                if (!regex.test(input.value)) {
-                    input.classList.add('invalid');
-                    allValid = false;
-                }
-            }
-
-            if (allValid) {
-                formData += '<h1>Введені дані:</h1>';
-                formData += `<p><strong>ПІБ:</strong> ${document.getElementById('fullName').value}</p>`;
-                formData += `<p><strong>ID-card:</strong> ${document.getElementById('idCard').value}</p>`;
-                formData += `<p><strong>Факультет:</strong> ${document.getElementById('faculty').value}</p>`;
-                formData += `<p><strong>Дата народження:</strong> ${document.getElementById('birthDate').value}</p>`;
-                formData += `<p><strong>Адреса:</strong> ${document.getElementById('address').value}</p>`;
-
-                const newWindow = window.open('', 'form_data', 'width=400,height=400');
-                newWindow.document.write(formData);
-            } else {
-                alert('Будь ласка, виправте помилки у виділених полях.');
+        Object.keys(fields).forEach(id => {
+            const input = document.getElementById(id);
+            if (input) {
+                input.classList.remove('invalid');
             }
         });
-    }
+
+        for (const id in fields) {
+            const input = document.getElementById(id);
+            const regex = fields[id];
+
+            if (!regex.test(input.value)) {
+                input.classList.add('invalid');
+                allValid = false;
+            }
+        }
+
+        if (allValid) {
+            formData += '<h1>Введені дані:</h1>';
+            formData += `<p><strong>ПІБ:</strong> ${document.getElementById('fullName').value}</p>`;
+            formData += `<p><strong>ID-card:</strong> ${document.getElementById('idCard').value}</p>`;
+            formData += `<p><strong>Факультет:</strong> ${document.getElementById('faculty').value}</p>`;
+            formData += `<p><strong>Дата народження:</strong> ${document.getElementById('birthDate').value}</p>`;
+            formData += `<p><strong>Адреса:</strong> ${document.getElementById('address').value}</p>`;
+
+            const newWindow = window.open('', 'form_data', 'width=400,height=400');
+            newWindow.document.write(formData);
+        } else {
+            alert('Будь ласка, виправте помилки у виділених полях.');
+        }
+    });
 
 
     const tableContainer = document.getElementById('tableContainer');
